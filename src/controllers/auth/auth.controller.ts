381891c0af8053c29549356284e7b147424f59c3
Hrita <hritadutta@gmail.com>
2026-08-28 00:47:14 +0545

    add email verification

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
 import { registerSchema } from "./auth.schema";
 import { User } from "../../models/user.model";
-import bcrypt from "bcryptjs";
 import { hashPassword } from "../../lib/hash";
+import jwt from "jsonwebtoken";
+import { sendEmail } from "../../lib/email";
+
+function getAppUrl() {
+  return process.env.APP_URL || `http://localhost:${process.env.PORT}`;
+}
 
 export async function registerHandler(req: Request, res: Response) {
   try {
@@ -43,5 +48,86 @@ export async function registerHandler(req: Request, res: Response) {
       name,
       twoFactorEnabled: false,
     });
-  } catch (err) {}
+
+    // email verfication part
+
+    const verifyToken = jwt.sign(
+      {
+        sub: newlyCreatedUser.id,
+      },
+      process.env.JWT_ACCESS_SECRET!,
+      {
+        expiresIn: "1d",
+      },
+    );
+
+    // creating email verify link
+    const verifyUrl = `${getAppUrl}/auth/verify-email?token=${verifyToken}`;
+
+    // send email to the user
+    await sendEmail(
+      newlyCreatedUser.email, // email
+      "Verify your email", // subject
+      `<p>Please verify your email by clicking this link:</p>
+        <p><a href="${verifyUrl}">${verifyUrl}</a></p>
+        `,
+    );
+
+    return res.status(201).json({
+      message: "User registered",
+      user: {
+        id: newlyCreatedUser.id,
+        email: newlyCreatedUser.email,
+        role: newlyCreatedUser.role,
+        isEmailVerified: newlyCreatedUser.isEmailVerified,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+}
+
+export async function verifyEmailHandler(req: Request, res: Response) {
+  // token from link query
+  const token = req.query.token as string | undefined;
+
+  if (!token) {
+    return res.status(400).json({
+      message: "Verification token is missing",
+    });
+  }
+
+  // email token verify
+  try {
+    const payload = jwt.verify(token, process.env.JWT_ACCESS_SECRET!) as {
+      sub: string;
+    };
+
+    // finding user with the ID from DB
+    const user = await User.findById(payload.sub);
+    if (!user) {
+      return res.status(400).json({ message: "User not found!" });
+    }
+
+    // User verfification check
+    if (user.isEmailVerified) {
+      return res.json({ message: "Email is already verified" });
+    }
+
+    // updating email verified status for a user
+    user.isEmailVerified = true;
+    await user.save();
+
+    return res.json({ message: "Email is now verified! You can login" });
+  } catch (err) {
+    console.log(err);
+
+    return res.json(500).json({
+      message: "Internal server error",
+    });
+  }
 }
