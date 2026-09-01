37beadbad7e8bf6b9802aa839443c40cf588ec10
Hrita <hritadutta@gmail.com>
2026-09-01 14:02:37 +0545

    add auth middleware

---
+++ src/middleware/requireAuth.ts
@@ -0,0 +1,51 @@
+import { NextFunction, Request, Response } from "express";
+import { verifyAccessToken } from "../lib/token";
+import { User } from "../models/user.model";
+
+async function requireAuth(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      message: "You are not auth user!",
+    });
+  }
+
+  const token = authHeader && authHeader.split(" ")[1];
+
+  try {
+    const payload = verifyAccessToken(token);
+
+    const user = await User.findById(payload.sub);
+
+    if (!user) {
+      return res.status(401).json({
+        message: "User not found!",
+      });
+    }
+
+    if (user.tokenVersion !== payload.tokenVersion) {
+      return res.status(401).json({
+        message: "Token invalidated",
+      });
+    }
+
+    const authReq = req as any;
+
+    authReq.user = {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      role: user.role,
+      isEmailVerfied: user.isEmailVerified,
+    };
+
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      message: "Invalid token",
+    });
+  }
+}
+
+export default requireAuth;
